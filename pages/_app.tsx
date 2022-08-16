c38c571367bf2b03d9ae801c8cf3bcdc52dd01f6
edwardez <11089470+edwardez@users.noreply.github.com>
2022-08-15 23:09:49 -0700

    Use scss and set up lint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
+import 'normalize.css/normalize.css';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
