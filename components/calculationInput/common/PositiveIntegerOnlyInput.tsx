77b3ee2503465161df649b1b2d458a901a4ebc41
edwardez <11089470+edwardez@users.noreply.github.com>
2022-09-26 23:56:37 -0700

    Add a common positive integer only component

---
+++ components/calculationInput/common/PositiveIntegerOnlyInput.tsx
@@ -0,0 +1,52 @@
+import {TextField} from '@mui/material';
+import {Controller, FieldValues, Path} from 'react-hook-form';
+import React from 'react';
+import {useTranslation} from 'next-i18next';
+import {Control} from 'react-hook-form/dist/types/form';
+
+interface PositiveIntegerOnlyInputProps<T extends FieldValues> {
+  name: Path<T>;
+  control: Control<T>;
+  showError: boolean;
+  helperText: string;
+}
+
+const PositiveIntegerOnlyInput = function<T>({
+  name, control, showError, helperText,
+}: PositiveIntegerOnlyInputProps<T>) {
+  const {t} = useTranslation('home');
+  return <Controller
+    name={name}
+    control={control}
+    rules={{
+      required: {
+        value: true,
+        message: t('addPieceDialog.required'),
+      },
+      pattern: {
+        value: /^\d+$/,
+        message: t('addPieceDialog.mustBeAInteger'),
+      },
+      min: {
+        value: 1,
+        message: t('addPieceDialog.minimumIs', {min: 1}),
+      },
+      max: {
+        value: 999,
+        message: t('addPieceDialog.maximumIs', {max: 999}),
+      },
+    }}
+    render={({field}) => (
+      <TextField
+        {...field}
+        inputProps={{pattern: '\\d*'}}
+        variant="outlined"
+        error={showError}
+        helperText={helperText}
+        label={t('addPieceDialog.quantity')}
+      />
+    )}
+  />;
+};
+
+export default PositiveIntegerOnlyInput;
