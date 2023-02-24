281a49f2b5d30414d5328d49fd57eeadd9fbfa05
edwardez <11089470+edwardez@users.noreply.github.com>
2023-02-24 12:00:33 -0800

    Mark rewardsByRegion as optional for supporting apps that have not received new data.

--- common/gameDataHandlerUtil.ts
+++ common/gameDataHandlerUtil.ts
@@ -4,5 +4,5 @@ import {GameServer} from 'model/Equipment';
 export const getRewardsByRegion = (campaign: Campaign, gameServer: GameServer) => {
   // try to get per region data, if not available, fallback to the legacy rewards.
   // `GameServer.Japan` contains the generic data.
-  return campaign.rewardsByRegion[gameServer] ?? campaign.rewardsByRegion[GameServer.Japan] ?? campaign.rewards;
+  return campaign?.rewardsByRegion[gameServer] ?? campaign?.rewardsByRegion[GameServer.Japan] ?? campaign.rewards;
 };
