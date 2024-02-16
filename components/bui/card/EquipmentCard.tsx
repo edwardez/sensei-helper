64d8e20fe567608189e5b1c5289fd49753a5c99e
rassvet2 <dekabrist19480705@gmail.com>
2024-02-16 17:55:08 +0900

    Adjust  layout of equipment cards

--- components/bui/card/EquipmentCard.tsx
+++ components/bui/card/EquipmentCard.tsx
@@ -4,32 +4,21 @@ import EquipmentImage from 'components/bui/card/EquipmentImage';
 import BuiCard from 'components/bui/BuiCard';
 import {CardActionArea} from '@mui/material';
 
+const EquipmentCard = ({
+  imageName, bottomLeftText, bottomRightText,
+  onClick, isSelected = false, hasOuterMargin = false,
+}: {
+  imageName: string, bottomLeftText?:string, bottomRightText?:string,
+  onClick?: MouseEventHandler, isSelected?: boolean, hasOuterMargin?: boolean,
+}) => {
+  return <BuiCard onClick={onClick}
+    className={`${hasOuterMargin ? styles.outerMargin : ''} ${isSelected ? 'wiz-selected' : ''}`}>
 
-const EquipmentCard = ({imageName, bottomLeftText, bottomRightText, onClick,
-  isSelected = false,
-  hasOuterMargin = false}:
-                           {imageName: string,
-                             onClick?: MouseEventHandler,
-                             bottomLeftText?:string,
-                             bottomRightText?:string,
-                             isSelected?: boolean,
-                             hasOuterMargin?: boolean}) => {
-  const equipmentContent = <React.Fragment>
-    <EquipmentImage imageName={imageName} />
-    {bottomLeftText ? <div className={styles.bottomLeftText}>{bottomLeftText}</div> :
-        null}
-    {bottomRightText ? <div className={styles.bottomRightText}>{bottomRightText}</div> :
-          null}</React.Fragment>;
-
-  return <BuiCard className={`${hasOuterMargin ? styles.outerMargin : ''} ${isSelected ? 'wiz-selected' : ''}`}>
-    {
-      onClick ?
-          <CardActionArea onClick={onClick} className={'exclude-revert-transform'}>
-            <div className={'revert-wiz-transform'}>
-              {equipmentContent}
-            </div>
-          </CardActionArea>:equipmentContent
-    }
+    <CardActionArea disabled={!onClick} className='exclude-revert-transform'>
+      <div className='revert-wiz-transform'><EquipmentImage imageName={imageName}/></div>
+      {bottomLeftText && <div className={styles.bottomLeftText}>{bottomLeftText}</div>}
+      {bottomRightText && <div className={styles.bottomRightText}>{bottomRightText}</div>}
+    </CardActionArea>
   </BuiCard>;
 };
 
